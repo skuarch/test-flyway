51dcbd015ef06b7865bace186586edd9382264c9
skuarch <skuarch@yahoo.com.mx>
2020-01-13 20:49:01 -0600

    New Project, only with flyway and postgres, for testing

---
+++ src/main/java/test/flyway/model/entity/Book.java
@@ -0,0 +1,16 @@
+package test.flyway.model.entity;
+
+// import grails.gorm.annotation.Entity;
+
+// @Entity
+class Book {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/test/flyway/model/entity/Station.java
@@ -0,0 +1,12 @@
+package test.flyway.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "station")
+public class Station {
+
+
+
+}
